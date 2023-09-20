4fddffe86368b853685c8b73c03f97567ebe42c9
Sylakh <sylvainsds@gmail.com>
2023-09-20 08:47:13 +0200

    add methods and correction

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -3,42 +3,69 @@ package com.hemebiotech.analytics;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * classe qui implemente l'interface ISymptonWriter doit ecrire le contenu d'une
  * MAP<String, Integer> dans un fichier result.out se poser la question de
  * l'ordre d'ecriture (ordre d'entree ou possibilité d'ecrire en fct de l'ordre
- * alphabetique?) pas besoin du filepath, le fichier sera creer dans la fct main
- * gerer les exceptions avec un try catch
+ * alphabetique? besoin du filepath, le fichier sera creer dans la fct main
+ * gerer les exceptions avec un try catch getter and setter?
  */
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
-	private Map<String, Integer> listSymptom = new HashMap<String, Integer>();
-	private String filepath;
+	private Map<String, Integer> listSymptom;
+	private String filePath;
 
-	public WriteSymptomDataToFile(HashMap<String, Integer> listSymptom) {
+	public WriteSymptomDataToFile(Map<String, Integer> listSymptom, String filePath) {
 		this.listSymptom = listSymptom;
+		this.filePath = filePath;
+	}
+
+	public Map<String, Integer> getListSymptom() {
+		return listSymptom;
+	}
+
+	public void setListSymptom(Map<String, Integer> listSymptom) {
+		this.listSymptom = listSymptom;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
 	}
 
 	@Override
 	public void writeSymptoms(Map<String, Integer> symptoms) {
 		if (symptoms != null) {
+			FileWriter file;
+			BufferedWriter writeSymptoms = null;
+
 			try {
-				FileWriter file = new FileWriter(filepath, false);
-				BufferedWriter writeSymptoms = new BufferedWriter(file);
+				file = new FileWriter(filePath, false);
+				writeSymptoms = new BufferedWriter(file);
 
 				for (Map.Entry<String, Integer> entry : listSymptom.entrySet()) {
 					String key = entry.getKey();
 					Integer value = entry.getValue();
 					writeSymptoms.write(key + ": " + value + "\n");
 				}
-				writeSymptoms.close();
 
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.err.println("error while writing file " + e.getMessage() + "caused by" + e.getCause());
+			} finally {
+				try {
+					if (writeSymptoms != null) {
+						writeSymptoms.close();
+					}
+
+				} catch (IOException e) {
+					System.err.println("error while closing file " + e.getMessage() + "caused by" + e.getCause());
+				}
 			}
 
 		}
